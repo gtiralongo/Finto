1f85a996e440389a78074a2e19f2eff1fd4850d5
gtiralongo <gustavotiralongo@gmail.com>
2026-04-16 15:01:15 -0300

    0.1.1 PWA

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'finto-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './firebase-config.js',
+  './manifest.json',
+  './logo.svg'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate & Cleanup Old Caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Strategy: Network First, Fallback to Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
